cec6a2b039968b36752a5cf6cde9db56c55ca28b
Adnan Hussain <axhussain@gmail.com>
2023-04-23 10:55:20 +0100

    2 - Implement `products/most-expensive` endpoint

--- ToolsBazaar.Web/Controllers/ProductsController.cs
+++ ToolsBazaar.Web/Controllers/ProductsController.cs
@@ -1,7 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToolsBazaar.Domain.ProductAggregate;
 
 namespace ToolsBazaar.Web.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class ProductsController : ControllerBase { }
+public class ProductsController : ControllerBase 
+{
+    private readonly IProductRepository _productRepository;
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(IProductRepository productRepository, ILogger<ProductsController> logger)
+    {
+        _productRepository = productRepository;
+        _logger = logger;
+    }
+
+    [HttpGet("most-expensive")]
+    public IOrderedEnumerable<Product> GetMostExpensive()
+    {
+        _logger.LogInformation("Retriving the most expensive products...");
+
+        var products = _productRepository
+            .GetAll()
+            .OrderByDescending(x => x.Price)
+            .ThenBy(x => x.Name);
+
+        return products;
+    }
+}
