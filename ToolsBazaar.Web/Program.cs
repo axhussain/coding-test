a395f40cf93638f12a45f472c48c0a8e1ba3dfd8
Adnan Hussain <axhussain@gmail.com>
2023-04-23 10:15:40 +0100

    1A - Fix update customer endpoint

--- ToolsBazaar.Web/Program.cs
+++ ToolsBazaar.Web/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Localization;
+using ToolsBazaar.Domain.CustomerAggregate;
 using ToolsBazaar.Domain.ProductAggregate;
 using ToolsBazaar.Persistence;
 
@@ -7,11 +9,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services .AddScoped<ICustomerRepository, CustomerRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment()) { }
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en-US")
+});
 
 app.UseRouting();
 
